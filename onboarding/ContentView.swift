e2bb118f97914b89d7bbedaa8b278b3ed74c81f1
faizmaruf <faiz.maruf8888@gmail.com>
2024-01-26 16:28:54 +0700

    Initial Commit

---
+++ onboarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  onboarding
+//
+//  Created by Faiz Ma'ruf on 26/01/24.
+//  Copyright © 2024 developer-rshb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
